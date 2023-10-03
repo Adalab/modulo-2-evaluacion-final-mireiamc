7455bcafec0e5cc5f102dbf2aaa5a806818d4332
¨mireiamc¨ <mireia.martin.carmona@gmail.com>
2023-10-03 21:42:07 +0200

    trying some excercises

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 // Variables de elementos HTML
@@ -8,6 +7,8 @@ const finderBtn = document.querySelector('.js-finder-btn');
 const searchListUl = document.querySelector('.js-search-list');
 const searchListText = document.querySelector('.js-coincidences_text');
 const resetBtn = document.querySelector('.js-reset-btn');
+// const logBtn = document.querySelector('.log__btn');
+// const numSearchResults = document.querySelector('.js-number-results');
 
 // Variables
 let searchList = [];
@@ -17,7 +18,7 @@ let favsList = savedFavs;
 if (savedFavs === null) {
   favsList = [];
 }
-
+// Tuve que hacer la condicion del null porque si no, me daba el error de que el array (cuando haciamos el renderFavsList) con el que abria la pagina por primera vez, no era iterable.
 renderFavsList();
 
 // Funciones
@@ -37,9 +38,22 @@ function handleClick(event) {
       } else {
         searchListText.innerHTML = 'Aquí están tus coincidencias:';
       }
+      // Printa en el numero de resultados que nos da data.
+      //   numSearchResults.innerHTML = `Tienes ${data.length} resultados`;
+
       renderSearchList(searchList);
     });
+
+  //   // Tecnica: Evento para clicar encima de los resultados
+  //   numSearchResults.addEventListener('click', numberOfResults);
 }
+
+// Tecnica: funcion manejadora del evento de arriba para que printe en consola el nombre de las series.
+// function numberOfResults() {
+//   for (const item of searchList) {
+//     console.log(item.show.name);
+//   }
+// }
 
 // Funcion para pintar las series filtradas y condicion para sustituir la imagen si no tiene.
 
@@ -72,6 +86,36 @@ function renderSearchList(searchList) {
     h3Title.appendChild(textTitleH3);
     elementContainer.appendChild(h3Title);
 
+    // Elemento nuevo para la tecnica
+
+    // const h3Date = document.createElement('h3');
+    // const textDateH3 = document.createTextNode(
+    //   `Fecha de estreno: ${item.show.premiered}`
+    // );
+    // h3Date.appendChild(textDateH3);
+    // elementContainer.appendChild(h3Date);
+
+    // Entrevista tecnica: poner el idioma, si no hay idioma no poner nada y ademas si es en ingles ponerlo como recomendada.
+    // if (item.show.language !== null) {
+    //   const h3Lang = document.createElement('h3');
+    //   const textLangH3 = document.createTextNode(
+    //     `Idioma: ${item.show.language}`
+    //   );
+    //   h3Lang.appendChild(textLangH3);
+    //   elementContainer.appendChild(h3Lang);
+    // }
+
+    // if (
+    //   item.show.language === 'English' ||
+    //   item.show.language === 'Spanish' ||
+    //   item.show.language === 'Portuguese'
+    // ) {
+    //   const recomended = document.createElement('p');
+    //   const textRecomended = document.createTextNode(`Serie recomendada`);
+    //   recomended.appendChild(textRecomended);
+    //   elementContainer.appendChild(recomended);
+    // }
+    //Busco si hay algun item con un id en favsList que me coincida con aluguno de los id de los items de la busqueda. En el caso de que coincida alguno, lel añado las clases que haran que me cambie de color la letra y el fondo de la tarjeta.
     const existingFav = favsList.find((fav) => item.show.id === fav.show.id);
     //Devuelve undefined cuando no encuentra ningun resultado que coincida.
     if (existingFav !== undefined) {
@@ -116,12 +160,20 @@ function renderFavsList() {
     deleteBtn.appendChild(deleteBtnText);
     liElement.appendChild(deleteBtn);
     deleteBtn.classList.add('delete-btn');
-    deleteBtn.showId = item.show.id; // Para conseguir que el boton tenga el id del item y poder eliminarlo - stackoverflow.com.
+    deleteBtn.showId = item.show.id; // Para conseguir que el boton tenga el id del item y poder eliminarlo - stackoverflow.com. No utilizo el id pq no puedo tener el mismo id en dos elementos diferentes del HTML, no puedo tener un boton con 123 y un li con un id 123.
 
+    // logBtn.addEventListener('click', logFavs); //Evento boton log entrevista tecnica
     deleteBtn.addEventListener('click', removeFav);
   }
 }
+// funcion prueba entrevista tecnica
 
+// function logFavs() {
+//   for (const item of favsList) {
+//     console.log(`Serie favorita: ${item.show.name}`);
+//   }
+//   console.log(favsList.length);
+// }
 // Funcion para quitar favoritos con el boton borrar
 
 function removeFav(event) {
@@ -154,23 +206,27 @@ function removeAllFavs() {
   storeFavs();
 }
 
-// Funcion manejadora del evento click para series favoritas.
+// Funcion manejadora del evento click para añadir o quitar de favoritos el item clicado.
 function clickFavs(event) {
+  // Guardo en itemClickedId el id del item que he clicado
   const itemClickedId = event.currentTarget.id;
+  // guardo en itemClicked el elemento del array de la lista de busqueda (searchList), que coincida con el id del item clicado. El toString lo necesito pq el id que leo es un string, por lo que el id del array tengo que pasarlo a string. 
   const itemClicked = searchList.find(
     (item) => item.show.id.toString() === itemClickedId
   );
-
+  // Hago una variable y busco con find en el array de favoritos si el id del item clicado coincide con alguno de los que tengo guardado en mi array favsList.
   const existingFav = favsList.find(
     (item) => itemClicked.show.id === item.show.id
   );
+
+  // Si existingFav me devuelve undefined, singifica que no tengo ningun favorito que contenga ese id en mi array, por lo que podremos añadirlo haciendo push. Por lo contrario, si ya existe, le diremos que lo elimine del array de favoritos.
   if (existingFav === undefined) {
     favsList.push(itemClicked);
   } else {
     const indexOfFav = favsList.indexOf(existingFav);
     favsList.splice(indexOfFav, 1);
   }
-
+  // Renderizamos de nuevo las funciones del FavsList, renderSearchList y storeFavs para que procesen de nuevo la informacion con las actualizaciones que se hayan podido hacer y se podamos visualizar el resultado con los cambios correspondientes.
   renderFavsList();
   renderSearchList(searchList);
   storeFavs();
